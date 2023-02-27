767aa38fc804f2683c23765d8afd6bfb7039156d
Michael Smith <ms193221@pennwest.edu>
2023-02-27 10:03:39 -0500

    reordered functions

--- src/functions.cpp
+++ src/functions.cpp
@@ -5,10 +5,10 @@ std::vector<std::string> functions;
 void setupFunctions()
 {
     /* Adds a keyword into the keywords vector. */
-    functions.push_back("run");
     functions.push_back("call");
+    functions.push_back("compile");
+    functions.push_back("run");
     functions.push_back("del");
     functions.push_back("print");
     functions.push_back("simple_compile");
-    functions.push_back("compile");
 }
